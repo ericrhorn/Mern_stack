1d461d34befdc92eedae223aaf101a6fcd8f1dc4
Eric Horn <hornericr@me.com>
2021-09-03 16:35:10 -0700

    first-jsx

---
+++ first-jsx/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <div className="list">
+    <h1 >Hello Dojo!</h1>
+      <h2>Things I need to do:</h2>
+      <ul>
+        <li>Learn React</li>
+        <li>Climb Mount Everest</li>
+        <li>Run a Marathon</li>
+        <li>Feed the Dogs</li>
+      </ul>
+    </div>
+    </>
+  );
+}
+
+export default App;
