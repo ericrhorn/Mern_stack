a5eed0f224329d0f492b75408fa49e2ab5d35b94
Eric Horn <hornericr@me.com>
2021-09-03 14:52:28 -0700

    hw

---
+++ Arrow/practice.js
@@ -0,0 +1,31 @@
+const person = {
+    firstName: 'Bob',
+    lastName: 'Marley',
+    email: 'bob@marley.com',
+    password: 'sekureP@ssw0rd9',
+    username: 'barley',
+    addresses: [
+    {
+        address: '1600 Pennsylvania Avenue',
+        city: 'Washington, D.C.',
+        zipcode: '20500',
+    },
+    {
+        address: '221B Baker St.',
+        city: 'London',
+        zipcode: 'WC2N 5DU',
+    }
+    ],
+    createdAt: 1543945177623
+}
+const {firstName} = person;
+console.log(person);
+
+const animals = ['horse', 'dog', 'fish', 'cat', 'bird'];
+
+const [firstAnimal, secondAnimal, ...otherAnimals] = animals;
+
+console.log(animals);
+
+
+
