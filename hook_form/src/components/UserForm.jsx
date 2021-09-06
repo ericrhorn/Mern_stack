8a21d42f52b56823b1846f19fc179328fd10e486
Eric Horn <hornericr@me.com>
2021-09-05 23:00:43 -0700

    hookForm

---
+++ hook_form/src/components/UserForm.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from  'react';
+    
+const UserForm = (props) => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");  
+    const [confPassword, setConfPassword] = useState("");  
+    
+    const createUser = (e) => {
+        // we must prevent the default refresh of the browser to keep our state from being reset
+        e.preventDefault();
+    
+        // create a javascript object to hold all of the values
+        const newUser = { firstName, lastName, email, password, confPassword };
+        console.log("Welcome", newUser);
+        setFirstName("");
+        setLastName("");
+        setEmail("");
+        setPassword("");
+        setConfPassword("");
+    };
+    
+    return(
+        <>
+        <form onSubmit={ createUser }>
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirstName(e.target.value)} value ={firstName} />
+            </div>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLastName(e.target.value)} value ={lastName} />
+            </div>
+            <div>
+                <label>Email Address: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value)} value ={email} />
+            </div>
+            <div>
+                <label>Password: </label>
+                <input type="text" onChange={ (e) => setPassword(e.target.value)} value = {password} />
+            </div>
+            <div>
+                <label>Confirm Password: </label>
+                <input type="text" onChange={ (e) => setConfPassword(e.target.value)} value = {confPassword} />
+            </div>
+            <input type="submit" value="Create User" />
+        </form>
+        <h4>Form Data</h4>
+        <p>First Name: {firstName}</p>
+        <p>Last Name: {lastName}</p>
+        <p>Email: {email}</p>
+        <p>Password: {password}</p>
+        <p>Confirm Pass: {confPassword}</p>
+        </>
+    );
+};
+    
+export default UserForm;
