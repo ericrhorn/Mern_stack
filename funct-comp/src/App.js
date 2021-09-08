48bd9907ee7b2dd1dbe4cc01a4d87bf6aba9598a
Eric Horn <hornericr@me.com>
2021-09-08 14:29:08 -0700

    update

--- funct-comp/src/App.js
+++ funct-comp/src/App.js
@@ -9,21 +9,25 @@ function App() {
         lastName={"Doe"}
         age={"45"}
         hair={"Black"}/>
+        <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
       <Person 
         firstName={"John"} 
         lastName={"Smith"}
         age={"88"}
         hair={"Brown"}/>
+        <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
       <Person 
         firstName={"Millard"} 
         lastName={"Fillmore"}
         age={"50"}
         hair={"Brown"}/>
+        <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
       <Person 
         firstName={"Maria"} 
         lastName={"Smith"}
         age={"62"}
         hair={"Brown"}/>
+        <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button>
     </div>
   );
 }
