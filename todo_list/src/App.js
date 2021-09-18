2a907b2e560f749b0473b69faed486985897db0a
Eric Horn <hornericr@me.com>
2021-09-17 18:22:23 -0700

    todo

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -1,33 +1,47 @@
-import React, {useState} from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [todoArr, setTodoArr] = useState([]);
   const [todo, setTodo] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    
-    if (todo.length === 0){
+
+    if (todo.length === 0) {
       return;
     }
-     
-    setTodoArr ([...todoArr, todo]);
-    setTodo("")
+
+    const newTodo = {
+      text: todo,
+      conplete: false,
+    };
+
+    setTodoArr([...todoArr, newTodo]);
+    setTodo("");
   };
 
   const handleDeleteTodo = (index) => {
     const deleteTodoArr = todoArr.filter((item, idx) => idx !== index);
-      setTodoArr(deleteTodoArr);
-  }
+    setTodoArr(deleteTodoArr);
+  };
+
+    const handleCheckbox = (index) => {
+      const checkTodo = { ...todoArr[index]};
+        checkTodo.complete = !checkTodo.complete;
+        setTodoArr([...todoArr.slice(0, index), checkTodo].concat(todoArr.slice(index + 1)));
+    }
 
   return (
     <div className="App">
       <form onSubmit={handleSubmit}>
         <div>
           <h1>Add To The List</h1>
-          <input type="text" onChange={(e) => setTodo(e.target.value)} value={todo}></input>
+          <input
+            type="text"
+            onChange={(e) => setTodo(e.target.value)}
+            value={todo}
+          ></input>
           <button type="submit">Add</button>
           {/* <input type="submit" value="Add"/> */}
         </div>
@@ -36,11 +50,15 @@ function App() {
       {todoArr.map((todo, index) => (
         <div>
           <p>
-          <span>{todo}</span>
-          <input type="checkbox"/>
-          <button onClick={() => handleDeleteTodo(index)}>
-          Delete
-          </button>
+            <span style={{ textDecoration: todo.complete && "line-through" }}>
+              {todo.text}
+            </span>
+            <input
+              type="checkbox"
+              checked={todo.complete}
+              onChange={() => handleCheckbox(index)}
+            />
+            <button onClick={() => handleDeleteTodo(index)}>Delete</button>
           </p>
         </div>
       ))}
