ffc466ac5e0ca46299f0ec425121619cafbdbb04
Eric Horn <hornericr@me.com>
2021-10-02 15:00:05 -0700

    update

--- jokes_api/server/models/jokes.model.js
+++ jokes_api/server/models/jokes.model.js
@@ -1,11 +1,12 @@
 const mongoose = require("mongoose");
 
-const jokeSchema = new mongoose.Schema({
+const jokeSchema = new mongoose.Schema(
+    {
 	setup: String,
 	punchline: String,
-    createdAt: Date,
-    updatedAt: Date,
-});
+    },
+    {timestamps: true}
+);
 
 const Joke = mongoose.model("Joke", jokeSchema);
 
