ff4b0aa4f246bf524bc19a79bb6ca0b730bce57a
Eric Horn <hornericr@me.com>
2021-10-19 16:27:58 -0700

    super ninja

--- ninja/ninja.js
+++ ninja/ninja.js
@@ -3,11 +3,11 @@ class Ninja {
     this.name = name;
     this.health = health;
     this.speed = speed;
-    this.stength = strength;
+    this.strength = strength;
   }
 
   sayName() {
-    console.log("this ninjas name is" + this.name);
+    console.log("this ninjas name is " + this.name);
   }
 
   showStats() {
@@ -19,18 +19,39 @@ class Ninja {
 
   drinkSake() {
     this.health += 10;
-    console.log(this.name + "ninja health is now" + this.health);
+    console.log("Bottoms up!, " + this.name + " ninja health is now " + this.health);
   }
 }
 
-const ninja1 = new Ninja("John", 5, 5, 10);
+class Sensei extends Ninja {
+    constructor (name, health = 200, speed = 10, strength = 10, wisdom = 10){
+        super (name, health, speed, strength);
+        this.wisdom = wisdom;
+    }
+    speakWisdom(){
+        this.drinkSake();
+        console.log("wise words")
+    }
+}
+    
+
+const ninja1 = new Ninja("John", 20, 10, 5);
 
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
+console.log(ninja1);
 
-const ninja2 = new Ninja("Joe", 10, 10, 20);
+const ninja2 = new Ninja("Joe", 20, 10);
 
 ninja2.sayName();
 ninja2.showStats();
 ninja2.drinkSake();
+console.log(ninja2);
+
+const sensei1 = new Sensei("darel", 30, 50, 100);
+
+sensei1.sayName();
+sensei1.showStats();
+sensei1.speakWisdom();
+console.log(sensei1);
